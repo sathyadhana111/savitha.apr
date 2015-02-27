bc09fba5ec7188c8cceb7849487f0d27ce0a775b
sathya <sathya@Ashwanths-MacBook-Pro.local>
2015-02-27 10:37:57 -0800

    Added weight.sh file in git repository.

---
+++ weight.sh
@@ -0,0 +1,13 @@
+#! /bin/bash
+
+weight=$1
+height=$2
+idealweight=[ $height - 110 ]
+
+if [ $weight - le $idealweight ]
+then
+  echo "You have to eat little more fat food."
+else
+  echo "You have to eat vegetables."
+fi
+
